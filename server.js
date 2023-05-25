27a09144f4c00c5f55e3af339749025db8cc2b08
Pavel Mikhaylenko <104693653+westteern@users.noreply.github.com>
2023-05-25 17:06:40 +0300

    add base
    server work

---
+++ server.js
@@ -0,0 +1,24 @@
+const app = require('./app');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3001;
+const uriDb = process.env.DB_HOST;
+
+mongoose.set('strictQuery', true);
+
+const connection = mongoose.connect(uriDb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+connection
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log('Database connection successful');
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.log(`Server not running. Error message: ${err.message}`);
+    process.exit(1);
+  });
